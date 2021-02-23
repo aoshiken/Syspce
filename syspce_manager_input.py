562f4d6a21c4e3c88e62eda1811dfeb3bc5218d8
aoshiken <aoshiken@users.noreply.github.com>
2021-02-23 10:33:20 +0100

    Let the program run without Volatility support Minor cosmetic changes

--- syspce_manager_input.py
+++ syspce_manager_input.py
@@ -5,7 +5,14 @@ from syspce_input_evtx import InputEvtx
 from syspce_input_eventlog import InputEventlog
 from syspce_manager import Manager_
 from syspce_message import *
-#from syspce_input_volatility import InputVolatility
+
+try:
+    import volatility.conf
+    g_use_volatility = True
+except:
+    print("Missing Volatility dependency, going on without Volatility support...")
+    g_use_volatility = False
+
 
 log = logging.getLogger('sysmoncorrelator')
 
@@ -13,8 +20,7 @@ class InputManager(Manager_):
 
     def __init__(self, data_buffer_in, data_condition_in):
 
-        Manager_.__init__(self, data_buffer_in,
-                                   data_condition_in)
+        Manager_.__init__(self, data_buffer_in, data_condition_in)
 
         self.name = 'Input Manager'
         self.module_id = Module.INPUT_MANAGER
@@ -31,42 +37,41 @@ class InputManager(Manager_):
 
             elif message._subtype == MessageSubType.READ_FROM_FILE:
                 self._read_evtx( message._src,
-                                                message._content[0],
-                                                message._content[1])
+                                 message._content[0],
+                                 message._content[1])
 
             elif message._subtype == MessageSubType.READ_FROM_EVENTLOG:
-                self._read_eventlog(message._src,
-                                                        message._content[0])
+                self._read_eventlog(message._src, message._content[0])
 
-            #elif message._subtype == MessageSubType.READ_FROM_MEMDUMP:
-            #    self._read_memdump(message._src,
-            #                                       message._content[0],
-            #                                       message._content[1],
-            #                                       message._content[2])
+            elif g_use_volatility and message._subtype == MessageSubType.READ_FROM_MEMDUMP:
+                self._read_memdump(message._src,
+                                   message._content[0],
+                                   message._content[1],
+                                   message._content[2])
 
     def _read_evtx(self, src, filepath, schema):
         input_evtx = InputEvtx(self.data_buffer_in,
-                                                   self.data_condition_in,
-                                                   src,
-                                                   filepath,
-                                                   schema)
+                               self.data_condition_in,
+                               src,
+                               filepath,
+                               schema)
 
         input_evtx.start()
         self.add_working_module(src, [input_evtx])
 
     def _read_eventlog(self, src, schema):
         input_eventlog = InputEventlog(self.data_buffer_in,
-                                                                   self.data_condition_in,
-                                                                   src,
-                                                                   schema)
+                                       self.data_condition_in,
+                                       src,
+                                       schema)
         input_eventlog.start()
         self.add_working_module(src, [input_eventlog])
 
-#    def _read_memdump(self, src, memdump, profile, memcache):
-#
-#        input_memdump = InputVolatility(self.data_buffer_in,
-#                                                                        self.data_condition_in,
-#                                                                        src, memdump, profile, memcache)
-#        input_memdump.start()
-#
-#        self.add_working_module(src, [input_memdump])
+    def _read_memdump(self, src, memdump, profile, memcache):
+
+        input_memdump = InputVolatility(self.data_buffer_in,
+                                        self.data_condition_in,
+                                        src, memdump, profile, memcache)
+        input_memdump.start()
+
+        self.add_working_module(src, [input_memdump])
